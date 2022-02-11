0829bc931e123697e962b30ad9319b80f2548d2c
Yang JoungBeom <very2sky@naver.com>
2022-02-11 12:30:30 +0900

    대문 페이지와 자기소개 페이지만들기

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path ,include
 from . import views
 
 urlpatterns = [
     path('<int:pk>/', views.single_post_page),
     path('', views.index),
+    path('', include('single_pages.urls')),
 ]
