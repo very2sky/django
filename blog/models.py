721bd3075576889e8e6636b181b1fdd0a488c300
Yang JoungBeom <very2sky@naver.com>
2022-02-18 12:54:02 +0900

    검색과 아바타

--- blog/models.py
+++ blog/models.py
@@ -74,4 +74,8 @@ class Comment(models.Model):
 
     def get_absolute_url(self):
         return f'{self.post.get_absolute_url()}#comment-{self.pk}'
-
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://doitdjango.com/avatar/id/574/9b54fc1350f80bda/svg/{self.author.email}'
